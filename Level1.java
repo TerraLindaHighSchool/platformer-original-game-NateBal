cb04f35297722d0ff5233800a2a64e7d72de901f
NateBal <89489235+NateBal@users.noreply.github.com>
2021-09-23 15:19:51 -0700

    Milestone 3 Complete

--- Level1.java
+++ Level1.java
@@ -8,6 +8,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Level1 extends World
 {
+    private final float GRAVITY = 0.0667f;
+    private final GreenfootSound MUSIC = new GreenfootSound("zapsplat_024.mp3");
     /**
      * Constructor for objects of class BrickWorld.
      * 
@@ -18,6 +20,12 @@ public class Level1 extends World
         super(1200, 800, 1, false); 
         prepare();
     }
+    
+    public void act()
+    {
+        spawn();
+    }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
@@ -27,8 +35,41 @@ public class Level1 extends World
         setPaintOrder(Player.class, Platform.class, Obstacle.class, Collectable.class,
               Door.class, HUD.class);
         Door door = new Door();
-        addObject(door,1137,50);
+        addObject(new Door(), 1137, 50);
         Player player = new Player();
-        addObject(player,53,662);
+        addObject(new Player(), 53, 662);
+        addObject(new Bomb(GRAVITY), 225, 775);
+        addObject(new Bomb(GRAVITY), 725, 775);
+        addObject(new Bomb(GRAVITY), 1075, 375);
+        addObject(new BrickWall(), 1125, 100);
+        addObject(new BrickWall(), 1175, 100);
+        addObject(new BrickWall(), 275, 700);
+        addObject(new BrickWall(), 425, 650);
+        addObject(new BrickWall(), 475, 650);
+        addObject(new BrickWall(), 325, 550);
+        addObject(new BrickWall(), 425, 500);
+        addObject(new BrickWall(), 575, 450);
+        addObject(new BrickWall(), 775, 450);
+        addObject(new BrickWall(), 725, 400);
+        addObject(new BrickWall(), 775, 250);
+        addObject(new BrickWall(), 1075, 200);
+        addObject(new BrickWall(), 875, 400);
+        addObject(new BrickWall(), 925, 250);
+        addObject(new Floor(), 600, 800);
+        SmBrickWall smBrickWall = new SmBrickWall();
+        addObject(new Trapdoor(GRAVITY), 575, 600);
+        addObject(new Trapdoor(GRAVITY), 925, 400);
+    }
+    
+    private void spawn()
+    {
+        if(Math.random() < 0.01)
+        {
+            addObject(new AcidRain(GRAVITY), Greenfoot.getRandomNumber(1200), -30);
+        }
+        if(Math.random() < 0.0025)
+        {
+            addObject(new Rock(GRAVITY), Greenfoot.getRandomNumber(1200), -30);
+        }
     }
 }
--- Trapdoor.java
+++ Trapdoor.java
@@ -8,12 +8,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Trapdoor extends Obstacle
 {
-    /**
-     * Act - do whatever the Trapdoor wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private float yVelocity;
+    private final float GRAVITY;
+    
+    public Trapdoor(float gravity)
+    {
+        GRAVITY = gravity;
+    }
+    
     public void act()
     {
-        // Add your action code here.
+        
     }
+    
+    protected void fall(){} 
 }
